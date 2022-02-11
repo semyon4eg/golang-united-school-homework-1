72225f7ad52e6e0513d4eac03e4ddbd383887dbf
Andrey Semenov <semenowav@gmail.com>
2022-02-12 01:42:50 +0300

    Fix: Edited solution.go

--- solution.go
+++ solution.go
@@ -1,15 +1,9 @@
-package main
+package solution
 
 import (
-	"fmt"
 	"github.com/kyokomi/emoji"
 )
 
-func main() {
-	helloMessage := emoji.Sprint(GetMessage())
-	fmt.Println(helloMessage)
-}
-
 func GetMessage() string {
-	return "Hello :world_map:!"
+	return emoji.Sprint("Hello :world_map:!")
 }
